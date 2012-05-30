1af28d13653926fb7d748b10e5ab0270c0d3c055
eskerda <eskerda@gmail.com>
2012-05-30 18:59:39 +0200

    Get token to load XML url.
    This emulates exactly a browser, not sure why this gets 404d

--- lib/domoblue.py
+++ lib/domoblue.py
@@ -1,8 +1,13 @@
 from station import Station
 from BeautifulSoup import BeautifulStoneSoup
 import urllib, urllib2
+import re
 
-URL = "http://clientes.domoblue.es/onroll/generaXml.php?cliente=%d"
+HOST = "http://clientes.domoblue.es/onroll/"
+SERVICE_URL = HOST+"generaXml.php?token={token}&cliente={client_id}"
+
+TOKEN_URL = HOST+"generaMapa.php?cliente={client_id}"
+TOKEN_RE = "generaXml\.php\?token\=(.*?)\&cliente"
 
 SERVICES = {
 	'albacete':{'id':2,'name':'albacete'},
@@ -49,33 +54,94 @@ SERVICES = {
 	'vinaros':{'id':51,'name':'vinaros'}
 }
 
-def get_all(prefix = ""):
+
+"""
+This headers are persistent between calls, use them in
+every call, in the state they are.
+"""
+
+headers = {
+	'User-Agent': 'CityBikes',
+	'Accept': '*/*'
+}
+
+opener = urllib2.build_opener()
+
+
+def request(url):
+	req = urllib2.Request(url, headers = headers)
+	response = opener.open(req)
+	head = response.info()
+	if 'set-cookie' in head:
+		headers['Cookie'] = head['set-cookie']
+	return response.read()
+
+def get_xml(client_id):
+	token = get_token(client_id)
+	url = SERVICE_URL.format(token = token, client_id = client_id)
+	return request(url)
+
+def get_all():
 	stations = []
 	for service in SERVICES:
-		for station in get_all_service(service,prefix):
+		for station in get_all_service(SERVICES[service]):
 			stations.append(station)
 	return stations
-def get_all_service(service,prefix = ""):
-	ID = SERVICES[service].get('id')
-	NAME = SERVICES[service].get('name')
-	request = urllib.urlopen(prefix+URL%ID)
-	xml_data = request.read()
-	request.close()
+
+def get_token(client_id):
+	url = TOKEN_URL.format(client_id = client_id)
+	html_data = request(url)
+	res = re.findall(TOKEN_RE, html_data)
+	return res[0]
+
+def get_services():
+	xml_data = get_xml('todos')
+	dom = BeautifulStoneSoup(xml_data)
+	markers = dom.findAll('marker')
+	services = {}
+	for marker in markers:
+		tmp = {
+			'id': marker['codigocliente'],
+			'name': marker['nombre']
+		}
+		tmp['service'] = slugfy(tmp['name'], '_')
+		services[tmp['service']] = tmp;
+	return services
+
+def get_all_service(service):
+	ID = service.get('id')
+	NAME = service.get('service')
+	xml_data = get_xml(ID)
 	dom = BeautifulStoneSoup(xml_data)
 	markers = dom.findAll('marker')
 	stations = []
 	for index, marker in enumerate(markers):
-		station = DomoBlueStation(index,NAME)
+		station = DomoBlueStation(index,str(NAME))
 		station.from_xml(marker)
 		stations.append(station)
 	return stations 
 
-class DomoBlueStation(Station):
-	def __init__(self, idx, pfx):
-		Station.__init__(self,idx)
-		self.prefix = pfx
 
-	def update(self, prefix=""):
+def slugfy(text, separator):
+  ret = ""
+  for c in text.lower():
+    try:
+      ret += htmlentitydefs.codepoint2name[ord(c)]
+    except:
+      ret += c
+ 
+  ret = re.sub("([a-zA-Z])(uml|acute|grave|circ|tilde|cedil)", r"\1", ret)
+  ret = re.sub("\W", " ", ret)
+  ret = re.sub(" +", separator, ret)
+ 
+  return ret.strip()
+
+class DomoBlueStation(Station):
+	def __init__(self, idx, prefix):
+		Station.__init__(self,idx)
+		self.prefix = prefix
+
+	def update(self):
 		return self
 
 	def from_xml(self, xml_data):
