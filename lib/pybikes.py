e91e607ed757eb5fe4a3067d734f22e42e1ac5e9
eskerda <eskerda@gmail.com>
2012-06-24 19:13:45 +0200

    Add an abstraction for a bike sharing system
    This is the first of a series of commits related to
    version 2.0
    
    With this new milestone, we are intending to clear and
    correct some syntaxes and limitations on the original
    pybikes library.
    
    It's been two years, and we are wiser now than before.

---
+++ lib/pybikes.py
@@ -0,0 +1,83 @@
+"""PyBikes
+PyBikes is a parsing library to extract bike sharing information from multiple
+sources. It contains multiple classes to handle this sort of information,
+and is not a class itself. The idea is to be able to call it like:
+
+from pybikes import BicingShareSystem, BicingStation
+
+bicing = new BicingShareSystem() <- Returns BicingShareSystem
+print "%s: %s" % ("Bicing City is", bicing.meta.city)
+stations = bicing.get_stations() <- Returns Array[BicingStation]
+
+Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+
+import sys
+from datetime import datetime
+import json
+
+__author__ = "eskerda (eskerda@gmail.com)"
+__version__ = "2.0"
+__copyright__ = "Copyright (c) 2010-2012 eskerda"
+__license__ = "AGPL"
+
+
+class BikeShareSystem(object):
+    """A base class to name a bike sharing System. It can be:
+        - Specific (cities):
+            - Bicing, Velib, ...
+        - General (companies):
+            - JCDecaux, ClearChannel
+        At the same time, these classes can extend their base class,
+        for example, Velib extends JCDecaux extends BikeShareSystem.
+
+        This class might or not have METADATA assigned, usually intended
+        for specific cases. This METADATA is dict / json formatted.
+    """
+    tag = None
+
+    meta = {
+        'name' : None,
+        'city' : None,
+        'country' : None,
+        'latitude' : None,
+        'longitude' : None,
+        'company' : None
+    }
+
+    def __init__(self, tag, meta):
+        
+        self.tag = tag
+        self.meta = dict(self.meta.items() + meta.items())
+
+    def __str__(self):
+        base = """--- {name} ---
+Uname: {uname}
+City: {city}
+Country: {country}
+LatLng: {latitude} / {longitude}
+Company: {company}
+"""
+        return base.format(
+                name = self.meta.get('name'),
+                city = self.meta.get('city'),
+                country = self.meta.get('country'),
+                latitude = self.meta.get('latitude'),
+                longitude = self.meta.get('longitude'),
+                company = self.meta.get('company'),
+                uname = self.tag
+            )
