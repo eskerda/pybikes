223275ff0ee7eee760e418bb30583c1d1d320932
eskerda <eskerda@gmail.com>
2012-11-15 04:47:09 +0100

    Port bixi from minidom to pyquery

--- pybikes/bixi.py
+++ pybikes/bixi.py
@@ -18,6 +18,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from xml.dom import minidom
+from pyquery import PyQuery as pq
 
 from .base import BikeShareSystem, BikeShareStation
 from . import utils
@@ -42,9 +43,10 @@ class BixiSystem(BikeShareSystem):
     def update(self):
 
         xml_data = self._scrapper.request(self.feed_url).read()
-        dom = minidom.parseString(xml_data)
-        markers = dom.getElementsByTagName('station')
+        dom = pq(xml_data, parser = 'xml')
+        markers = dom('station')
         stations = []
+        
         for index, marker in enumerate(markers):
             station = BixiStation(index)
             station.from_xml(marker)
@@ -59,39 +61,39 @@ class BixiStation(BikeShareStation):
     def from_xml(self, xml_data):
         """ xml marker object as in
         <station>
-        <id>1</id>
-        <name>Notre Dame / Place Jacques Cartier</name>
-        <terminalName>6001</terminalName>
-        <lat>45.508183</lat>
-        <long>-73.554094</long>
-        <installed>true</installed>
-        <locked>false</locked>
-        <installDate>1276012920000</installDate>
-        <removalDate />
-        <temporary>false</temporary>
-        <nbBikes>14</nbBikes>
-        <nbEmptyDocks>17</nbEmptyDocks>
+            <id>1</id>
+            <name>Notre Dame / Place Jacques Cartier</name>
+            <terminalName>6001</terminalName>
+            <lat>45.508183</lat>
+            <long>-73.554094</long>
+            <installed>true</installed>
+            <locked>false</locked>
+            <installDate>1276012920000</installDate>
+            <removalDate />
+            <temporary>false</temporary>
+            <nbBikes>14</nbBikes>
+            <nbEmptyDocks>17</nbEmptyDocks>
         </station>
         """
-
-        terminalName = str(utils.getTextTag(xml_data, 'terminalName'))
-        name = str(utils.getTextTag(xml_data, 'name'))
+        xml_data = pq(xml_data, parser='xml')
+        
+        terminalName = xml_data('terminalName').text()
+        name = xml_data('name').text()
         self.name = "%s - %s" % (terminalName, name)
-
-        self.latitude = float(utils.getTextTag(xml_data, 'lat'))
-        self.longitude = float(utils.getTextTag(xml_data, 'long'))
-
-        self.bikes = int(utils.getTextTag(xml_data, 'nbBikes'))
-        self.free = int(utils.getTextTag(xml_data, 'nbEmptyDocks'))
+        self.latitude = float(xml_data('lat').text())
+        self.longitude = float(xml_data('long').text())
+        self.bikes = int(xml_data('nbBikes').text())
+        self.free = int(xml_data('nbEmptyDocks').text())
 
         self.extra = {
-            'uid': int(utils.getTextTag(xml_data, 'id')),
-            'name' : name,
+            'uid': int(xml_data('id').text()),
+            'name': name,
             'terminalName' : terminalName,
-            'locked': utils.str2bool(utils.getTextTag(xml_data, 'locked')),
-            'installed': utils.str2bool(utils.getTextTag(xml_data, 'installed')),
-            'temporary': utils.str2bool(utils.getTextTag(xml_data, 'temporary')),
-            'installDate': utils.getTextTag(xml_data, 'installDate'),
-            'removalDate': utils.getTextTag(xml_data, 'removalDate'),
-            'latestUpdateTime': utils.getTextTag(xml_data, 'latestUpdateTime'),
+            'locked': utils.str2bool(xml_data('locked').text()),
+            'installed': utils.str2bool(xml_data('installed').text()),
+            'temporary': utils.str2bool(xml_data('temporary').text()),
+            'installDate': xml_data('installDate').text(),
+            'removalDate': xml_data('removalDate').text(),
+            'latestUpdateTime': xml_data('latestUpdateTime').text()
         }
+        return self
