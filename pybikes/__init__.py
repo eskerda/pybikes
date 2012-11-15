c5da9b2b785c38c504e96276b5029290dbabe9e6
eskerda <eskerda@gmail.com>
2012-11-15 06:59:19 +0100

    Update copyright headers and license to AGPL

--- pybikes/__init__.py
+++ pybikes/__init__.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
+# Distributed under the AGPL license, see LICENSE.txt
+
 """PyBikes
 PyBikes is a parsing library to extract bike sharing information from multiple
 sources. It contains multiple classes to handle this sort of information,
@@ -9,21 +12,6 @@ from pybikes import BicingShareSystem, BicingStation
 bicing = new BicingShareSystem() <- Returns BicingShareSystem
 print "%s: %s" % ("Bicing City is", bicing.meta.city)
 stations = bicing.get_stations() <- Returns Array[BicingStation]
-
-Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
+# Distributed under the AGPL license, see LICENSE.txt
+
 from setuptools import setup, find_packages
 
 setup(
