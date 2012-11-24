4e9824ebd6e66c79b413c22e4df354ecd0dbe53e
eskerda <eskerda@gmail.com>
2012-11-24 22:00:47 +0100

    Delete all old (v0.1) files
    Time to ditch all this old ugly code.

--- sys/midtub.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-DIRECTORY=`dirname $0`
-NOT_UPDATE=$1
-
-$DIRECTORY/threader.py system ecobici all 1 $NOT_UPDATE &
-$DIRECTORY/threader.py system bizi all 2 $NOT_UPDATE &
-$DIRECTORY/threader.py system bicing all 1 $NOT_UPDATE &
