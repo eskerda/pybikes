af4d910da5dd8f1596e24be70c193ddaed3941d5
eskerda <eskerda@gmail.com>
2013-03-26 22:59:28 +0100

    add support for comunes on Torino

--- lib/tobike.py
+++ lib/tobike.py
@@ -3,10 +3,44 @@ from bicincittanew import BicincittaNewStation
 import bicincittanew
 
 PREFIX = "tobike"
-URL = "http://www.tobike.it/frmLeStazioni.aspx"
+URL = "http://www.tobike.it/frmLeStazioni.aspx?ID={id}"
+COMUNES = [
+  {
+    'id': 22,
+    'name': 'Torino'
+  },
+  {
+    'id': 61,
+    'name': 'Grugilasco'
+  },
+  {
+    'id': 62,
+    'name': 'Collegno'
+  },
+  {
+    'id': 63,
+    'name': 'Venaria Reale'
+  },
+  {
+    'id': 64,
+    'name': 'Alpignano'
+  },
+  {
+    'id': 65,
+    'name': 'Druento'
+  }
+]
 
 def get_all():
-  return bicincittanew.get_all(TOBikeStation)
+  stations = []
+  st_range = 0
+  for comune in COMUNES:
+    spec = TOBikeStation
+    spec.main_url = URL.format(id = comune['id'])
+    comune_stations = bicincittanew.get_all(TOBikeStation, st_range)
+    st_range += len(comune_stations)
+    stations += comune_stations
+  return stations
 
 class TOBikeStation(BicincittaNewStation):
   prefix = PREFIX
