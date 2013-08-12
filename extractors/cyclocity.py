ad823939f68bd3d0140712254fa68a3aa614859c
eskerda <eskerda@gmail.com>
2013-08-12 19:12:47 +0200

    Update the cyclocity extractor script

--- extractors/cyclocity.py
+++ extractors/cyclocity.py
@@ -1,162 +1,97 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
+# Distributed under the AGPL license, see LICENSE.txt
+
 import os
 import sys
 import time
 import json
 import argparse
-import unidecode
 
-import requests
-from googlegeocoder import GoogleGeocoder
 from slugify import slugify
+import pybikes
 
-MAIN = 'https://gw.cyclocity.fr/3311a6cea2e49b10/'
+api_key = 'ace81338b73283277ddfe54c217ab965ac93cb50'
 
-ACTIONS = {
-    'cities': 'contracts/full?token={token}',
-    'token': 'token/key/b885ab926fdca7dbfbf717084fb36b5f',
-    'availability': 'availability/{city}/stations/state/?token={token}',
-    'availabitity_by_geo': 'availability/{city}/stations/proximity/{what}?lat={lat}&lng={lng}&maxRes=10000&min=1&token={token}'
-}
+description = 'Extract cyclocity instances'
 
-CYCLOCITY_ENDPOINT = "https://abo-{code}.cyclocity.fr"
+parser = argparse.ArgumentParser(description = description)
 
-TOKEN = None
+parser.add_argument('-o', metavar = "output", dest = "output", 
+                    type = argparse.FileType('w'), default = sys.stdout, 
+                    help="Output file")
 
-geocoder = GoogleGeocoder()
+parser.add_argument('-v', action="store_true", dest = 'verbose', 
+                    default = False, help="Verbose output for debugging (no progress)")
 
-cyclocity_data = {
+parser.add_argument('--proxy', metavar = "host:proxy", dest = 'proxy', 
+                    default = None, help="Use host:port as a proxy for site calls")
+
+parser.add_argument('--httpsproxy', metavar = "host:proxy", dest = 'httpsproxy', 
+                    default = None, help="Use host:port as an HTTPS proxy for site calls")
+
+args = parser.parse_args()
+
+scraper = pybikes.utils.PyBikesScraper()
+
+proxies = {}
+
+sysdef = {
     "system": "cyclocity",
     "class": "Cyclocity",
     "instances": []
 }
 
-parser = argparse.ArgumentParser(description = 'Extract Cyclocity instances from JCD')
-parser.add_argument('-o', metavar = "file", dest = 'outfile', default = None, 
-                    help="Save output to the specified file")
-parser.add_argument('-g','--geocode', action="store_true",
-                    help="Use Google GeoCoder for lat/lng and better names")
+def clearline(length):
+    clearline = "\r" + "".join([" " for i in range(length)])
+    sys.stderr.flush()
+    sys.stderr.write(clearline)
+    sys.stderr.flush()
 
-parser.add_argument('--proxy', metavar = "host:proxy", dest = 'proxy', default = None,
-                    help="Use host:port as a proxy for JCD calls")
+def print_status(i, total, status):
+    progress = "".join(["#" for step in range(i)]) + \
+               "".join([" " for step in range(total-i)])
+    status_pattern = "\r{0}/{1}: [{2}] {3}"
+    output = status_pattern.format(i, total, progress, status)
+    sys.stderr.flush()
+    sys.stderr.write(unicode(output))
+    sys.stderr.flush()
+    if (i == total):
+        sys.stderr.write('\n')
+    return len(output)
 
-args = parser.parse_args()
+def main():
+    if args.proxy is not None:
+        proxies['http'] = args.proxy
+        scraper.enableProxy()
 
-outfile = args.outfile
+    if args.httpsproxy is not None:
+        proxies['https'] = args.httpsproxy
+        scraper.enableProxy()
 
-session = requests.session()
+    scraper.setProxies(proxies)
 
-proxies = {}
+    services = pybikes.Cyclocity.get_contracts(api_key, scraper)
+    lastlen = 0
+    for i, service in enumerate(services):
+        sysdef['instances'].append(
+            {
+                'tag': slugify(service['commercial_name']),
+                'contract': service['name'],
+                'meta': {
+                    'name': service['commercial_name'],
+                    'country': service['country_code']
+                }
+            }
+        )
+        clearline(lastlen)
+        lastlen = print_status(i+1, len(services), \
+                        "Testing %s" % repr(service['name']))
 
-if args.proxy is not None:
-    proxies['http'] = args.proxy
+    output = json.dumps(sysdef, sort_keys = False, indent = 4)
+    args.output.write(output)
+    args.output.write('\n')
+    args.output.close()
 
-def getUrl(action, **args):
-    return '%s%s' % (MAIN, ACTIONS[action].format(**args))
-
-def call(action, **args):
-    if 'token' not in args and TOKEN is not None:
-        args['token'] = TOKEN
-    url = getUrl(action, **args)
-    r = requests.get(url, proxies = proxies)
-    return r.json()
-
-def getToken():
-    data = call('token')
-    return data['token']
-
-def fix_cergy(system):
-    system['code'] = 'cergy'
-    return system
-
-def fix_valence(system):
-    system['countryCode'] = 'ES'
-    system['name'] = 'Valencia'
-    return system
-
-hacks = {
-    'cergy-pontoise': fix_cergy,
-    'valence': fix_valence,
-}
-
-country_hash = {
-    'es': 'es',
-    'fr': 'fr',
-    'ru': 'ru',
-    'jp': 'ja',
-    'lt': 'lt'
-}
-
-
-def extract_instance( system ):
-    if system['code'] in hacks:
-        system = hacks[system['code']](system)
-    addr = system['name']
-    
-    try:
-        if not args.geocode:
-            raise Exception
-        print "Geocoding %s" % addr
-        geodata = geocoder.get(addr)
-        time.sleep(1)
-        latitude = geodata[0].geometry.location.lat
-        longitude = geodata[0].geometry.location.lng
-
-        if system['countryCode'].lower() in country_hash:
-            language = country_hash[system['countryCode'].lower()]
-        else:
-            language = 'en'
-        print "Reverse geocoding for better City names in %s" % language
-        geodata = geocoder.get((latitude, longitude), language = language)
-        time.sleep(1)
-        better_name = [component.long_name for component in geodata[1].address_components if 'locality' in component.types]
-        if len(better_name) > 0:
-            city = better_name[0]
-        else:
-            city = system['name']
-    except Exception as excp:
-        print "No geodata found for %s: %s" % (addr, excp)
-        print "Setting lat/lng to 0.0 for human intervention"
-        latitude = 0.0
-        longitude = 0.0
-        city = system['name']
-
-    tag = slugify(system['comname'])
-    if (tag == 'cyclocity'):
-        tag = slugify(system['name'])
-
-    return {
-        "tag": tag,
-        "root_url": CYCLOCITY_ENDPOINT.format(code = system['code']),
-        "city": system['code'],
-        "meta": {
-            "name": system['comname'],
-            "city": city,
-            "country": system['countryCode'],
-            "latitude": latitude,
-            "longitude": longitude
-        }
-    }
-
-print "Adquiring token..."
-TOKEN = getToken()
-print "Getting cities..."
-cities = call('cities')
-print "Got %d cities from cyclocity servers" % len(cities)
-
-for city in cities:
-    instance = extract_instance(city)
-    if instance is not None:
-        cyclocity_data['instances'].append(instance)
-
-cyclocity_data['instances'] = sorted(cyclocity_data['instances'], key = lambda inst: inst['city'])
-
-data = json.dumps(cyclocity_data, sort_keys = False, indent = 4)
-
-if outfile is not None:
-    f = open(outfile, 'w')
-    f.write(data)
-    f.close()
-    print "%s file written" % outfile
-else:
-    print "---- OUTPUT ----"
-    print data
+if __name__ == "__main__":
+    main()
