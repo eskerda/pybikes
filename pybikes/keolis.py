1d906e5cbd92c33838bbbb943944e3b89d2983a5
eskerda <eskerda@gmail.com>
2014-06-27 20:36:10 +0200

    Adds Keolis (v1?) + an instance (IDECycle)

--- pybikes/keolis.py
+++ pybikes/keolis.py
@@ -2,14 +2,101 @@
 # Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
 # Distributed under the AGPL license, see LICENSE.txt
 
+import re
+import json
+
+from lxml import etree
+import lxml.html
+
 from .base import BikeShareSystem, BikeShareStation
 from . import utils
 
-from lxml import etree
 
-__all__ = ['Keolis_v2', 'KeolisStation_v2']
+__all__ = ['Keolis', 'KeolisStation', 'Keolis_v2', 'KeolisStation_v2']
 
 xml_parser = etree.XMLParser(recover = True)
+_re_float = "([+-]?\\d*\\.\\d+)(?![-+0-9\\.])"
+
+class Keolis(BikeShareSystem):
+    sync = True
+
+    meta = {
+        'system': 'Keolis',
+        'company': 'Keolis'
+    }
+
+    _re_fuzzle = '\"latitude\"\:\ \"{0}\"\,\ '\
+                 '\"longitude\"\:\ \"{0}\"\,\ '\
+                 '\"text\"\:\ \"(.*?)\"\,\ '\
+                 '\"markername'.format(_re_float)
+    _re_num_name = "\#(\d+)\ \-\ (.*)" # #10 - Place Lyautey
+
+    def __init__(self, tag, meta, feed_url):
+        super(Keolis, self).__init__(tag, meta)
+        self.feed_url = feed_url
+
+    def update(self, scraper = None):
+        if scraper is None:
+            scraper = utils.PyBikesScraper()
+        raw_fuzzle = scraper.request(self.feed_url)
+        data = re.findall(Keolis._re_fuzzle, raw_fuzzle)
+        self.stations = map(KeolisStation, data)
+
+class KeolisStation(BikeShareStation):
+    def __init__(self, data):
+        """
+        fuzzle is something like
+        Must be utf8 encoded and string-escaped
+        <div class="gmap-popup">
+            <div class="gmap-infobulle">
+                <div class="gmap-titre">#16 - Universite Sud</div>
+                <div class="gmap-adresse">
+                    AVENUE DE L'UNIVERSITE FACE A L'ENTREE DE L'UNIVERSITE
+                </div>
+                <div class="gmap-velos">
+                    <table>
+                        <tr>
+                            <td class="ok">
+                                <strong>9</strong> vélos disponibles
+                            </td>
+                            <td class="ok">
+                                <strong>17</strong> places disponibles
+                            </td>
+                            <td>
+                                <acronym title="Carte Bancaire">CB</acronym>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="gmap-datemaj">
+                    dernière mise à jour il y a <strong>00 min</strong>
+                </div>
+            </div>
+        </div>
+        """
+        super(KeolisStation, self).__init__(0)
+        fuzzle = lxml.html.fromstring(
+            data[2].encode('utf8').decode('string-escape')
+        )
+        num_name = re.findall(
+            Keolis._re_num_name,
+            fuzzle.xpath('//div[@class="gmap-titre"]/text()')[0]
+        )[0]
+
+        bikes_places_upd = fuzzle.xpath('//strong/text()')
+
+        # Will not use
+        # address  = fuzzle.xpath('//div[@class="gmap-adresse"]/text()')[0]
+
+        self.latitude  = float(data[0])
+        self.longitude = float(data[1])
+        self.name      = num_name[1]
+        self.bikes     = int(bikes_places_upd[0])
+        self.free      = int(bikes_places_upd[1])
+        self.extra     = {
+            'uid': int(num_name[0])
+        }
+
 
 class Keolis_v2(BikeShareSystem):
 
