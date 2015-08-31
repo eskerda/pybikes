da5efe90c870735c9e14dfe5818282e144f6d212
Eduardo Mucelli Rezende Oliveira <edumucelli@gmail.com>
2015-08-31 15:57:54 +0200

    Characters limit about 80, and removing chain assignment

--- pybikes/samba.py
+++ pybikes/samba.py
@@ -44,13 +44,20 @@ class Samba(BaseSystem):
         # function is in the position of the array, and thus the [:-1]
         for station in stations[:-1]:
             data = station.split(',')
-            name, latitude, longitude, bikes, free, online_status, operation_status = data[3], float(data[0]), float(data[1]), int(data[7]), int(data[8]) - int(data[7]), data[5], data[6]
+            name = data[3]
+            latitude = float(data[0])
+            longitude = float(data[1])
+            bikes = int(data[7])
+            free = int(data[8]) - int(data[7])
+            online_status = data[5]
+            operation_status = data[6]
             extra = {
                 'address': data[9],
                 'uid': int(data[4]),
                 'slots': int(data[8])
             }
-            self.stations.append(SambaStation(name, latitude, longitude, bikes, free, online_status, operation_status, extra))
+            self.stations.append(SambaStation(name, latitude, longitude, bikes, free,
+                                              online_status, operation_status, extra))
 
 class SambaNew(BaseSystem):
     sync = True
@@ -70,18 +77,29 @@ class SambaNew(BaseSystem):
         stations_html = re.findall(SambaNew._STATIONS_RGX, html)
         stations = ast.literal_eval(stations_html[0])
         '''
-        Different from the original Samba class, th new one deals receives stations' information in the following format:
-        [(0) name, (1) latitude, (2) longitude, (3) address, (4) address main line, (5) onlineStatus, (6) operationStatus, (7) available bikes (variable not being used in their code) 
-            (8) available bikes, (9) available bike stands, (10) internal station status, (11) path to image file, (12) stationId]
+        Different from the original Samba class, th new one deals
+        receives stations' information in the following format:
+        [(0) name, (1) latitude, (2) longitude, (3) address,
+        (4) address main line, (5) onlineStatus, (6) operationStatus, 
+        (7) available bikes (variable not being used in their code) 
+        (8) available bikes, (9) available bike stands,
+        (10) internal station status, (11) path to image file, (12) stationId]
         '''
         self.stations = []
         for station in stations:
-            name, latitude, longitude, bikes, free, online_status, operation_status = station[0], station[1], station[2], station[8], station[9], station[5], station[6]
+            name = station[0]
+            latitude = station[1]
+            longitude = station[2]
+            bikes = station[8]
+            free = station[9]
+            online_status = station[5]
+            operation_status = station[6]
             extra = {
                 'address': station[4],
                 'description': station[3]
             }
-            self.stations.append(SambaStation(name, latitude, longitude, bikes, free, online_status, operation_status, extra))
+            self.stations.append(SambaStation(name, latitude, longitude, bikes, free,
+                                              online_status, operation_status, extra))
 
 class SambaStation(BikeShareStation):
     def __init__(self, name, latitude, longitude, bikes, free, online_status, operation_status, extra = {}):
