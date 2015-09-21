38416569f901b91778e1cdd9860298fc8cf4a213
eduardom <edumucelli@gmail.com>
2015-09-21 23:55:24 +0200

    Adding InvalidStation exception

--- pybikes/bcycle.py
+++ pybikes/bcycle.py
@@ -6,6 +6,7 @@ import re
 
 from lxml import html
 from .base import BikeShareSystem, BikeShareStation
+from .exceptions import InvalidStation
 from . import utils
 
 __all__ = ['BCycleSystem', 'BCycleStation']
@@ -14,15 +15,12 @@ LAT_LNG_RGX = "var\ point\ =\ new\ google.maps.LatLng\(([+-]?\\d*\\.\\d+)(?![-+0
 DATA_RGX = "var\ marker\ =\ new\ createMarker\(point\,(.*?)\,\ icon\,\ back"
 USERAGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/31.0.1650.63 Chrome/31.0.1650.63 Safari/537.36"
 
-
 class BCyclePurgatoryException(Exception):
     """
     Purgatory stations are only used for bikes that are lost, pending recovery.
     It has no bike slots so it shouldn't be considered a valid station.
     """
-
     pass
-
 
 class BCycleSystem(BikeShareSystem):
 
@@ -58,9 +56,8 @@ class BCycleSystem(BikeShareSystem):
         for latlng, fuzzle in zip(geopoints, puzzle):
             try:
                 self.stations.append(BCycleStation(latlng, fuzzle))
-            except BCyclePurgatoryException:
+            except (InvalidStation, BCyclePurgatoryException):
                 pass
-
 
 class BCycleStation(BikeShareStation):
     def __init__(self, latlng, fuzzle):
@@ -101,8 +98,7 @@ class BCycleStation(BikeShareStation):
 
             address = dom.xpath("//div[@class='markerAddress']/text()")
             availability = dom.xpath("//div[@class='markerAvail']//h3/text()")
-            """ Special events marker has no availability information, discard it
-                var marker = new createMarker(
+            """ var marker = new createMarker(
                     point, 
                     "<div class='markerTitle'>
                         <h3>Special Events 2015</h3>
@@ -118,7 +114,7 @@ class BCycleStation(BikeShareStation):
                     </div>", 
                     icon, back, false);"""
             if not availability:
-                raise BCyclePurgatoryException
+                raise InvalidStation
             bikes = availability[0]
             free = availability[1]
 
