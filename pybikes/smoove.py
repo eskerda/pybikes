d52eb61523b6cdd266c0db151cf73c0dc2b86038
eduardom <edumucelli@gmail.com>
2015-12-09 18:32:14 +0100

    Back to regex only

--- pybikes/smoove.py
+++ pybikes/smoove.py
@@ -10,16 +10,11 @@ from . import utils, exceptions
 
 __all__ = ['Smoove', 'SmooveStation']
 
-# Each station is formatted as:
-# newmark_01(
-#    28,
-#    45.770958,
-#    3.073871,
-#    "<div class=\"mapbal\" align=\"left\">022 Jaurès<br>Vélos disponibles: 4<br>Emplacements libres: 10<br>CB: Non<br></div>");
-# I.e., (station_id, latitude, longitude, status_html)
-# status_html contains name, available_bikes, free_bike_stands, credit_card_enabled (discarded afterwards)
-DATA_RGX = r'\(\d+\,\ (\d+.\d+).*?(\d+.\d+)\,\ "(.*?)"\)'
-STATUS_RGX = r'.*?\:\ (\d+)'
+# station_uid, latitude, longitude
+STATIONS_RGX = r'newmark_\d+\(\s*(\d+)\s*,\s*(\d+.\d+),\s*(\d+.\d+)\s*,\s*\"{}\"'
+# name, available_bikes, free_bike_stands, credit_card_enabled (discarded afterwards)
+STATUS_RGX = r'<div.*?>(.*?)<br>.*?:\s*(.*?)<br>.*?:\s*(.*?)<br>.*?:\s*(.*?)<br></div>'
+DATA_RGX = STATIONS_RGX.format(STATUS_RGX)
 
 class Smoove(BikeShareSystem):
 
@@ -41,16 +36,22 @@ class Smoove(BikeShareSystem):
         html = scraper.request(self.feed_url)
         stations_data = re.findall(DATA_RGX, html)
 
+        # Each station is formatted as:
+        # newmark_01(
+        #    28,
+        #    45.770958,
+        #    3.073871,
+        #    "<div class=\"mapbal\" align=\"left\">022 Jaurès<br>Vélos disponibles: 4<br>Emplacements libres: 10<br>CB: Non<br></div>"
+        # );
+
         stations = []
-        for station_data in stations_data:
-            latitude, longitude, status_raw_html = station_data
-            status_html = lxml.html.fromstring(status_raw_html)
-            # discards the last element of stations_data
-            # which indicates if the station is credit card-enabled
-            name, raw_bikes, raw_free = status_html.xpath('text()')[:-1]
-            free = int(re.findall(STATUS_RGX, raw_free)[0])
-            bikes = int(re.findall(STATUS_RGX, raw_bikes)[0])
+        # discards the last element of stations_data
+        # which indicates if the station is credit card-enabled
+        for uid, latitude, longitude, name, bikes, free, _ in stations_data:
+            extra = {
+                'uid': int(uid)
+            }
             station = BikeShareStation(name, float(latitude), float(longitude),
-                                    int(bikes), int(free))
+                                    int(bikes), int(free), extra)
             stations.append(station)
         self.stations = stations
