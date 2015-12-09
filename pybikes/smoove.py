4588e371235c584b782fb9fd6134115bc8f3d05b
eskerda <eskerda@gmail.com>
2015-12-09 18:59:33 +0100

    [smoove] Code cleanup

--- pybikes/smoove.py
+++ pybikes/smoove.py
@@ -3,18 +3,26 @@
 # Distributed under the AGPL license, see LICENSE.txt
 
 import re
-import lxml.html
 
 from .base import BikeShareSystem, BikeShareStation
-from . import utils, exceptions
+from . import utils
 
-__all__ = ['Smoove', 'SmooveStation']
+
+# Each station is formatted as:
+# newmark_01(
+#    28,
+#    45.770958,
+#    3.073871,
+#    "<div class=\"mapbal\" align=\"left\">022 Jaurès<br>Vélos disponibles: 4
+#    <br>Emplacements libres: 10<br>CB: Non<br></div>"
+# );
 
 # station_uid, latitude, longitude
-STATIONS_RGX = r'newmark_\d+\(\s*(\d+)\s*,\s*(\d+.\d+),\s*(\d+.\d+)\s*,\s*\"{}\"'
-# name, available_bikes, free_bike_stands, credit_card_enabled (discarded afterwards)
-STATUS_RGX = r'<div.*?>(.*?)<br>.*?:\s*(.*?)<br>.*?:\s*(.*?)<br>.*?:\s*(.*?)<br></div>'
+STATIONS_RGX = r'newmark_\d+\(\s*(\d+)\s*,\s*(\d+.\d+),\s*(\d+.\d+)\s*,\s*\"{}\"'  # NOQA
+# name, available_bikes, free_bike_stands, credit_card_enabled
+STATUS_RGX = r'<div.*?>(.*?)<br>.*?:\s*(.*?)<br>.*?:\s*(.*?)<br>.*?:\s*(.*?)<br></div>'  # NOQA
 DATA_RGX = STATIONS_RGX.format(STATUS_RGX)
+
 
 class Smoove(BikeShareSystem):
 
@@ -29,21 +37,12 @@ class Smoove(BikeShareSystem):
         super(Smoove, self).__init__(tag, meta)
         self.feed_url = feed_url
 
-    def update(self, scraper = None):
+    def update(self, scraper=None):
         if scraper is None:
             scraper = utils.PyBikesScraper()
-        
+
         html = scraper.request(self.feed_url)
         stations_data = re.findall(DATA_RGX, html)
-
-        # Each station is formatted as:
-        # newmark_01(
-        #    28,
-        #    45.770958,
-        #    3.073871,
-        #    "<div class=\"mapbal\" align=\"left\">022 Jaurès<br>Vélos disponibles: 4<br>Emplacements libres: 10<br>CB: Non<br></div>"
-        # );
-
         stations = []
         # discards the last element of stations_data
         # which indicates if the station is credit card-enabled
@@ -52,6 +51,6 @@ class Smoove(BikeShareSystem):
                 'uid': int(uid)
             }
             station = BikeShareStation(name, float(latitude), float(longitude),
-                                    int(bikes), int(free), extra)
+                                       int(bikes), int(free), extra)
             stations.append(station)
         self.stations = stations
