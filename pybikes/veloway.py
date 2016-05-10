66412d9b6b384411f2fae7b82718cb3a5cb63a63
eskerda <eskerda@gmail.com>
2016-05-10 14:35:38 +0200

    Fix dkvelo bike share system

--- pybikes/veloway.py
+++ pybikes/veloway.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
+# Copyright (C) 2010-2016, eskerda <eskerda@gmail.com>
 # Distributed under the AGPL license, see LICENSE.txt
 
+import re
 import json
 import urllib
+from lxml import html
 
 from .base import BikeShareSystem, BikeShareStation
 from . import utils
 
-__all__ = ['Veloway','VelowayStation']
 
 class BaseSystem(BikeShareSystem):
     meta = {
@@ -16,12 +17,13 @@ class BaseSystem(BikeShareSystem):
         'company': 'Veolia'
     }
 
+
 class Veloway(BaseSystem):
     def __init__(self, tag, meta, feed_url):
         super(Veloway, self).__init__(tag, meta)
         self.feed_url = feed_url
 
-    def update(self, scraper = None):
+    def update(self, scraper=None):
         if scraper is None:
             scraper = utils.PyBikesScraper()
 
@@ -34,6 +36,7 @@ class Veloway(BaseSystem):
                 continue
             stations.append(station)
         self.stations = stations
+
 
 class VelowayStation(BikeShareStation):
     """ A "stand" is like:
@@ -53,9 +56,9 @@ class VelowayStation(BikeShareStation):
     """
     def __init__(self, info):
         super(VelowayStation, self).__init__()
-        self.name = urllib.unquote_plus(info['name']
-                .decode('latin-1')
-                .encode('utf-8'))
+        self.name = urllib.unquote_plus(
+            info['name'].decode('latin-1').encode('utf-8')
+        )
         self.bikes = int(info['ab'])
         self.free = int(info['ap'])
         self.latitude = float(info['lat'])
@@ -72,11 +75,51 @@ class VelowayStation(BikeShareStation):
             self.extra['status'] = 'CLOSED'
 
         if info['wcom']:
-            self.extra['address'] = urllib.unquote_plus(info['wcom']
-                    .decode('latin-1')
-                    .encode('utf-8'))
+            self.extra['address'] = urllib.unquote_plus(
+                info['wcom'].decode('latin-1').encode('utf-8')
+            )
 
         if self.latitude is None or self.longitude is None:
             raise Exception('A station needs a lat/lng to be defined!')
         if self.latitude == 0 and self.longitude == 0:
             raise Exception('A station can\'t be located in Atlantic Ocean!')
+
+
+class VelowayDrupal(Veloway):
+
+    # Station 01 - Gare SNCF (Vélos libres : 9 Places libres
+    station_rgx = (ur'Station\s+(?P<id>\d+)\s*-\s*'
+                   ur'(?P<name>.+)\s*'
+                   ur'\(Vélos\s+libres\s*:\s*(?P<bikes>\d+)\s+'
+                   ur'Places\s+libres\s*:\s*(?P<free>\d+)\)')
+
+    def update(self, scraper=None):
+        scraper = scraper or utils.PyBikesScraper()
+
+        _html = scraper.request(self.feed_url)
+        dom = html.fromstring(_html)
+        # Data is duplicated...
+        markers = dom.xpath("""
+            (//ul[li[@data-gmapping]])[1]//
+                li[@data-gmapping]
+        """)
+        stations = []
+        for marker in markers:
+            g_map = json.loads(marker.get('data-gmapping'))
+            lat = float(g_map['latlng']['lat'])
+            lng = float(g_map['latlng']['lng'])
+
+            fuzzle = ''.join(marker.xpath('.//text()')).strip()
+            data = re.search(self.station_rgx, fuzzle)
+
+            uid = data.group('id')
+            name = data.group('name')
+            bikes = int(data.group('bikes'))
+            free = int(data.group('free'))
+
+            extra = {
+                'uid': uid
+            }
+            station = BikeShareStation(name, lat, lng, bikes, free, extra)
+            stations.append(station)
+        self.stations = stations
