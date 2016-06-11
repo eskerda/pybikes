0cd6fce73ccd2e62e2e7a83f7491025ad72ced08
eskerda <eskerda@gmail.com>
2016-06-11 21:00:03 +0200

    [bikeu] Fix some networks failing
    Some networks do have the map as an iframe inside the
    map.

--- pybikes/bikeu.py
+++ pybikes/bikeu.py
@@ -3,6 +3,9 @@
 # Distributed under the AGPL license, see LICENSE.txt
 import re
 import json
+from urlparse import urljoin
+
+from lxml import html
 
 from .base import BikeShareSystem, BikeShareStation
 from . import utils
@@ -22,13 +25,28 @@ class Bikeu(BikeShareSystem):
         super(Bikeu, self).__init__(tag, meta)
         self.url = url
 
+    def parse_map(self, body):
+        matched = re.search(REGEX, body)
+        if not matched:
+            return None
+        markers = json.loads(matched.group(1))
+        return markers
+
     def update(self, scraper=None):
         if scraper is None:
             scraper = utils.PyBikesScraper()
 
-        matched = re.search(REGEX,scraper.request(self.url))
-        markers = json.loads(matched.group(1))
-
+        body = scraper.request(self.url)
+        markers = self.parse_map(body)
+        if not markers:
+            # Map data is on an iframe
+            _html = html.fromstring(body)
+            map_src = next(iter(_html.xpath('id("MapData")/@src')), None)
+            if not map_src:
+                raise Exception('No marker data found')
+            map_src = urljoin(self.url, map_src)
+            map_body = scraper.request(map_src)
+            markers = self.parse_map(map_body)
         self.stations = map(BikeuStation, markers)
 
 
