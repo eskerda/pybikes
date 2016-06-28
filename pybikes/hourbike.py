15238d8a416aa6d10e90c0c697273b06dac7fe49
eduardom <edumucelli@gmail.com>
2016-06-28 23:53:55 +0200

    Add Hourbike (Reading, Liverpool, Liconln, Sheffield) #148

---
+++ pybikes/hourbike.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) Eduardo Mucelli Rezende Oliveira <edumucelli@gmail.com>
+# Distributed under the AGPL license, see LICENSE.txt
+
+import re
+import json
+
+from .base import BikeShareSystem, BikeShareStation
+from . import utils
+
+__all__ = ['Hourbike']
+
+RGX_MARKERS = r'Operator\.StationsData = (.*?)\;'
+
+class Hourbike(BikeShareSystem):
+
+    meta = {
+        'system': 'Hourbike',
+        'company': 'Hourbike'
+    }
+
+    def __init__(self, tag, feed_url, meta):
+        super(Hourbike, self).__init__(tag, meta)
+        self.feed_url = feed_url
+
+    def update(self, scraper=None):
+        if scraper is None:
+            scraper = utils.PyBikesScraper()
+
+        self.stations = []
+        html = scraper.request(self.feed_url)
+
+        raw_data = re.search(RGX_MARKERS, html).group(1)
+        stations_data = json.loads(raw_data)
+
+        for station in stations_data:
+            latitude = float(station['Latitude'])
+            longitude = float(station['Longitude'])
+
+            name = station['Name']
+            bikes = int(station['TotalAvailableBikes'])
+            free = int(station['TotalLocks']) - bikes
+            extra = {
+                'uid': station['id']
+            }
+            station = BikeShareStation(name, latitude, longitude, bikes, free,
+                                       extra)
+            self.stations.append(station)
