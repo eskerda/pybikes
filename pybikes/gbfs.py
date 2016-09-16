c30e58378b713a510482e23ebbfc00504be0f773
anastmag <anastasios@mageirias.com>
2016-09-15 23:42:06 -0400

    corrections, removed bcycle stations that overlap

--- pybikes/gbfs.py
+++ pybikes/gbfs.py
@@ -3,6 +3,7 @@
 # Distributed under the AGPL license, see LICENSE.txt
 
 import json
+import operator
 
 from .base import BikeShareSystem, BikeShareStation
 from . import utils
@@ -11,7 +12,11 @@ __all__=['Gbfs', 'GbfsStation']
 
 class Gbfs(BikeShareSystem):
 
+
     def __init__(self, tag, meta, feed_url):
+        # Add feed_url to meta in order to be exposed to the API
+        meta['gbfs_href']=feed_url
+
         super(Gbfs, self).__init__(tag, meta)
         self.feed_url=feed_url
 
@@ -24,48 +29,59 @@ class Gbfs(BikeShareSystem):
 
         # Create a dict with name-url pairs for easier access
         # of urls (just in case)
-
         feeds={}
         for feed in html_data['data']['en']['feeds']:
             feeds[feed['name']]=feed['url']
 
-        '''Station Information and Station Status data retrieval
-        '''
-        station_information=json.loads(scraper.request(feeds['station_information']))
-        station_status=json.loads(scraper.request(feeds['station_status']))
+        #Station Information and Station Status data retrieval
+        station_information=json.loads(scraper.request(feeds['station_information']))\
+            ['data']['stations']
+        station_status=json.loads(scraper.request(feeds['station_status']))\
+            ['data']['stations']
 
-        for info, status in zip(station_information['data']['stations'],\
-        station_status['data']['stations']):
-            self.stations.append(GbfsStation(info, status))
+        # Merge station_information and station_status into one dictionary for
+        # every station after sorting both by 'station_id' in order to avoid
+        # possible unmatches.
+        # Nabsa spec doesn't mention anything about stations order at the moment.
+        # Finally, pass the combined dictionary to GbfsStation as an argument.
+        sorting_key=operator.itemgetter('station_id')
+        station_information=sorted(station_information, key=sorting_key)
+        station_status=sorted(station_status, key=sorting_key)
+
+        self.stations=[]
+        for info, status in zip(station_information, station_status):
+            info.update(status)
+            self.stations.append(GbfsStation(info))
 
 
 class GbfsStation(BikeShareStation):
 
-    def __init__(self, info, status):
+    def __init__(self, info):
         """
         Example info variable:
-        {u'post_code': u'null', u'capacity': 31, u'name': u'Ft. York / Capreol Crt.',
-        u'cross_street': u'null', u'lon': -79.395954, u'station_id': u'7000', u'address':
-        u'Ft. York / Capreol Crt.', u'lat': 43.639832}
-
-        Example status variable:
-        {u'last_reported': 1473627463, u'num_bikes_disabled': 3, u'num_docks_available': 16,
-        u'is_renting': 1, u'station_id': u'7000', u'num_docks_disabled': 0, u'is_installed':
-        1, u'num_bikes_available': 12, u'is_returning': 1}
+        {u'is_installed': 1, u'post_code': u'null', u'capacity': 31,
+        u'name': u'Ft. York / Capreol Crt.', u'cross_street': u'null',
+        u'num_bikes_disabled': 0, u'last_reported': 1473969337,
+        u'lon': -79.395954, u'station_id': u'7000', u'is_renting': 1,
+        u'num_docks_available': 26, u'num_docks_disabled': 0,
+        u'address': u'Ft. York / Capreol Crt.', u'lat': 43.639832,
+        u'num_bikes_available': 5, u'is_returning': 1}
 
         So let's extract the dataaa
-
-        Note: Maybe it's valuable to check every time the station_ids to see if
-        they match.
         """
+
         super(GbfsStation, self).__init__()
 
-        self.station_id=[info['station_id'] if
-                info['station_id']==status['station_id'] else 'Wrong']
-        self.name=str(info.get('name'))
-        self.status=['Active' if status['is_renting'] and
-            status['is_installed'] else 'Inactive']
-        self.bikes=status.get('num_bikes_available')
-        self.free=status.get('num_docks_available')
-        self.latitude=float(info.get('lat'))
-        self.longitude=float(info.get('lon'))
+        self.name=str(info['name'])
+        self.bikes=int(info['num_bikes_available'])
+        self.free=int(info['num_docks_available'])
+        self.latitude=float(info['lat'])
+        self.longitude=float(info['lon'])
+        self.extra={
+                # address is optional
+                'address': info.get('address'),
+                'uid': info['station_id'],
+                'status': 'Online' if all([info['is_renting'], info['is_installed']])\
+                    else 'Offline',
+                'last_updated': int(info['last_reported'])
+                }
