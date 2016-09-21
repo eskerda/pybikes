86d9b63ae5e348fc56c62621f5faaebdc91fc703
anastmag <anastasios@mageirias.com>
2016-09-21 09:47:55 -0400

    [Baksi] remove city_uid
    * removed city_uid since it's not used
    * pep-8 fixes

--- pybikes/baksi.py
+++ pybikes/baksi.py
@@ -9,11 +9,11 @@ from . import utils
 
 __all__ = ['Baksi', 'BaksiStation']
 
-ID_NAME_RGX="([0-9]+)\-([\w\s.()-]+)\'"
-STATUS_RGX="Durum\ [&nbsp;]+\ (\w+)"
-DOCKS_RGX="Park[&nbsp;]+([0-9]+)"
-BIKES_RGX="Bisiklet[&nbsp;]+([0-9]+)"
-LAT_LNG_RGX="([\s0-9.]+)\',\ \'([\s0-9.]+)"
+ID_NAME_RGX = "([0-9]+)\-([\w\s.()-]+)\'"
+STATUS_RGX = "Durum\ [&nbsp;]+\ (\w+)"
+DOCKS_RGX = "Park[&nbsp;]+([0-9]+)"
+BIKES_RGX = "Bisiklet[&nbsp;]+([0-9]+)"
+LAT_LNG_RGX = "([\s0-9.]+)\',\ \'([\s0-9.]+)"
 
 class Baksi(BikeShareSystem):
 
@@ -22,45 +22,44 @@ class Baksi(BikeShareSystem):
         'company': ['Baksi Bike Sharing System']
     }
 
-    def __init__(self, tag, meta, city_uid, feed_url):
+    def __init__(self, tag, meta, feed_url):
         super(Baksi, self).__init__(tag, meta)
-        self.feed_url=feed_url
+        self.feed_url = feed_url
 
-    def update(self, scraper=None):
+    def update(self, scraper = None):
         if scraper is None:
             scraper = utils.PyBikesScraper()
 
-        html_data=scraper.request(self.feed_url, raw=True).decode('iso-8859-9')
+        html_data=scraper.request(self.feed_url, raw = True).decode('iso-8859-9')
 
-        """Fetch data
-        """
-        id_name=re.findall(ID_NAME_RGX, html_data, re.UNICODE)
-        status=re.findall(STATUS_RGX, html_data, re.UNICODE)
-        docks=re.findall(DOCKS_RGX, html_data, re.UNICODE)
-        bikes=re.findall(BIKES_RGX, html_data, re.UNICODE)
-        geopoints=re.findall(LAT_LNG_RGX, html_data, re.UNICODE)
+        # Fetch Data
+        id_name = re.findall(ID_NAME_RGX, html_data, re.UNICODE)
+        status = re.findall(STATUS_RGX, html_data, re.UNICODE)
+        docks = re.findall(DOCKS_RGX, html_data, re.UNICODE)
+        bikes = re.findall(BIKES_RGX, html_data, re.UNICODE)
+        geopoints = re.findall(LAT_LNG_RGX, html_data, re.UNICODE)
 
-        """Refine output
-        """
-        station_id, name=zip(*id_name)
-        status=["Active" if out=="Aktif" else "Inactive" for out in status]
-        docks=[int(i) for i in docks]
-        bikes=[int(i) for i in bikes]
-        latitude, longitude=zip(*geopoints)
+        # Refine Output
+        station_id, name = zip(*id_name)
+        status = ["Active" if out == "Aktif" else "Inactive" for out in status]
+        docks = [int(i) for i in docks]
+        bikes = [int(i) for i in bikes]
+        latitude, longitude = zip(*geopoints)
 
-        self.stations=map(BaksiStation, zip(station_id, name, status, docks, bikes, latitude, longitude))
+        self.stations = map(BaksiStation, zip(station_id, name, status, docks,
+            bikes, latitude, longitude))
 
 
 class BaksiStation(BikeShareStation):
     def __init__(self, data):
         super(BaksiStation, self).__init__()
-        self.name=data[1]
-        self.bikes=data[3]
-        self.free=data[4]
-        self.latitude=float(data[5])
-        self.longitude=float(data[6])
-        self.extra={
+        self.name = data[1]
+        self.bikes = data[3]
+        self.free = data[4]
+        self.latitude = float(data[5])
+        self.longitude = float(data[6])
+        self.extra = {
                 'uid': data[0],
                 'status': data[2],
-                'slots': int(data[3]+data[4]),
+                'slots': int(data[3] + data[4])
         }
