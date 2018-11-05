6b31cfeca0e093aa7ffc7cd2753f2a0f03a7372a
eskerda <eskerda@gmail.com>
2018-11-05 12:43:28 +0100

    Include UID field on stadtrad-hamburg

--- pybikes/stadtrad_hamburg.py
+++ pybikes/stadtrad_hamburg.py
@@ -1,23 +1,15 @@
 # -*- coding: utf-8 -*-
 # Distributed under the LGPL license, see LICENSE.txt
 
-import re
 import json
 
-from .base import BikeShareSystem, BikeShareStation
-from . import utils
-
-__all__ = ['StadtradHamburg', 'StadtradHamburgStation']
+from pybikes import BikeShareSystem, BikeShareStation, PyBikesScraper
 
 # license: https://www.govdata.de/dl-de/by-2-0
-BASE_URL = 'https://geodienste.hamburg.de/HH_WFS_Stadtrad?service=WFS&request=GetFeature&VERSION=1.1.0&typename' \
-           '=stadtrad_stationen&outputFormat=application/geo%2bjson&srsName=EPSG:4326'
+BASE_URL = 'https://geodienste.hamburg.de/HH_WFS_Stadtrad?service=WFS&request=GetFeature&VERSION=1.1.0&typename=stadtrad_stationen&outputFormat=application/geo%2bjson&srsName=EPSG:4326'   # NOQA
 
 
 class StadtradHamburg(BikeShareSystem):
-    sync = True
-    unifeed = True
-
     meta = {
         'system': 'stadtrad_hamburg',
         'company': ['Transparenzportal Hamburg'],
@@ -27,18 +19,11 @@ class StadtradHamburg(BikeShareSystem):
         }
     }
 
-    def __init__(self, tag, meta):
-        super(StadtradHamburg, self).__init__(tag, meta)
-        self.url = BASE_URL
-
     def update(self, scraper=None):
-        if scraper is None:
-            scraper = utils.PyBikesScraper()
-
-        api_response = json.loads(scraper.request(self.url))
-
+        scraper = scraper or PyBikesScraper()
+        api_response = json.loads(scraper.request(BASE_URL))
         self.stations = [
-            StadtradHamburgStation(feature) for feature in api_response['features']
+            StadtradHamburgStation(f) for f in api_response['features']
         ]
 
 
@@ -49,3 +34,4 @@ class StadtradHamburgStation(BikeShareStation):
         self.longitude = float(info['geometry']['coordinates'][0])
         self.name = info['properties']['name'].encode("utf-8")
         self.bikes = int(info['properties']['anzahl_raeder'])
+        self.extra = {'uid': info['properties']['uid']}
