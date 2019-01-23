1f8e164ea81d9ce60ac7320a851a3e294d368b46
eskerda <eskerda@gmail.com>
2019-01-23 14:15:36 +0100

    Fix bicing system using open data feed
    I guess when they finally move to PBSC system things will break up
    again, but worth fixing it on the meanwhile

---
+++ pybikes/bicing.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import json
+
+from pybikes import BikeShareSystem, BikeShareStation, PyBikesScraper
+
+
+class Bicing(BikeShareSystem):
+    meta = {
+        'system': 'Bicing',
+        'company': [
+            'Barcelona de Serveis Municipals, S.A. (BSM)',
+            'CESPA',
+            'PBSC',
+        ],
+    }
+
+    url = 'http://wservice.viabicing.cat/v2/stations'
+
+    def update(self, scraper=None):
+        scraper = scraper or PyBikesScraper()
+
+        self.stations = []
+
+        data = json.loads(scraper.request(self.url))
+        self.stations = [BicingStation(s) for s in data['stations']]
+
+
+class BicingStation(BikeShareStation):
+
+    name_format = u'{number} - {street} {street_number}'
+
+    def __init__(self, data):
+        super(BicingStation, self).__init__()
+        self.name = self.name_format.format(
+            number=data['id'],
+            street=data['streetName'],
+            street_number=data['streetNumber'],
+        )
+        self.latitude = float(data['latitude'])
+        self.longitude = float(data['longitude'])
+        self.bikes = int(data['bikes'])
+        self.free = int(data['slots'])
+        self.extra = {
+            'uid': int(data['id']),
+            'altitude': data['altitude'],
+            'status': data['status'],
+            # We keep this shitty name because of compatibility with the old
+            # feed
+            'NearbyStationList': [
+                int(uid.strip()) for uid in data['nearbyStations'].split(',')
+            ],
+            'has_ebikes': 'ELECTRIC' in data['type'],
+        }
