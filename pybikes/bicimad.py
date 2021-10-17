aadfbfb7e0261729c54701f9705835335ba28a73
Altonss <66519591+Altonss@users.noreply.github.com>
2021-10-17 16:56:11 +0200

    Update bicimad.py

--- pybikes/bicimad.py
+++ pybikes/bicimad.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018, eskerda <eskerda@gmail.com>
+# Copyright (C) 2021, Altonss (https://github.com/Altonss)
 # Distributed under the LGPL license, see LICENSE.txt
 
 import json
@@ -11,49 +12,25 @@ COLORS = ['green', 'red', 'yellow', 'gray']
 
 
 class Bicimad(BikeShareSystem):
-
-    authed = True
-
     meta = {
         'system': 'bicimad',
         'company': 'Empresa Municipal de Transportes de Madrid, S.A.'
     }
 
-    headers = {
-        'User-Agent': 'okhttp/3.3.0',
-        'Content-Type': 'application/json',
-    }
-
-    endpoint = 'https://api.bicimad.com/emt_v10/'
-
-    def __init__(self, tag, meta, key):
+    def __init__(self, tag, meta, feed_url):
         super(Bicimad, self).__init__(tag, meta)
-        self.app_hash = key
+        self.feed_url = feed_url
 
     def update(self, scraper=None):
         scraper = scraper or PyBikesScraper()
-        scraper.headers.update(self.headers)
-
-        payload = {
-            'app_hash': self.app_hash,
-            'compressed': True,
-            'function': 'get_stations',
-            'station': '0',
-        }
-
-        response = scraper.request(self.endpoint, 'POST',
-                                   data=json.dumps(payload))
-        # Double JSON is always better than single JSON :)
-        data = json.loads(json.loads(response)['data'])
-        self.stations = [BicimadStation(s) for s in data['stations']]
-
+        data = json.loads(scraper.request(self.feed_url))
+        self.stations = [BicimadStation(s) for s in data['data']]
 
 class BicimadStation(BikeShareStation):
     def __init__(self, data):
         super(BicimadStation, self).__init__()
         self.name = data['name']
-        self.latitude = float(data['latitude'])
-        self.longitude = float(data['longitude'])
+        self.longitude, self.latitude = map(float, data['geometry']['coordinates'])        
         self.bikes = int(data['dock_bikes'])
         self.free = int(data['free_bases'])
         self.extra = {
