2bf90fe76a0285bf1c5a6385a9ea9ad3ed5828ef
Altonss <66519591+Altonss@users.noreply.github.com>
2021-10-27 00:41:38 +0200

    Fix typo

--- pybikes/ecovelo.py
+++ pybikes/ecovelo.py
@@ -11,7 +11,7 @@ class Ecovelo(BikeShareSystem):
  
     meta = {
         'system': 'Ecovelo',
-        'company': ['Ecovelo']
+        'company': ['Ecovelo'],
     }
 
     BASE_URL = "https://api.cyclist.ecovelo.mobi/v2/stations?&program={dataset}&limit=100"
@@ -26,20 +26,18 @@ class Ecovelo(BikeShareSystem):
         data = json.loads(scraper.request(self.feed_url))
         stations = map(lambda r: r, data['data'])
         self.stations = list(map(EcoveloStation, stations))
-
+		
 
 class EcoveloStation(BikeShareStation):
     def __init__(self, fields):
-        name = fields['name']
-        latitude = float(fields['position']['latitude'])
-	longitude = float(fields['position']['longitude'])
-        bikes = int(fields['docks']['total_vehicules'])
-        free = int(fields['docks']['total_free'])
-        extra = {
+		name = fields['name']
+		latitude = float(fields['position']['latitude'])
+		longitude = float(fields['position']['longitude'])
+		bikes = int(fields['docks']['total_vehicules'])
+		free = int(fields['docks']['total_free'])
+		extra = {
             'status': fields['status'],
             'uid': str(fields['id']),
-            'online': fields['status'] == "open"
+            'online': fields['status'] == "open",
         }
-        super(EcoveloStation, self).__init__(name, latitude, longitude,
-                                                bikes, free, extra)
-
+		super(EcoveloStation, self).__init__(name, latitude, longitude, bikes, free, extra)
