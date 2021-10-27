f4594cf753868de611ae1a4bc507f697940f743a
Altonss <66519591+Altonss@users.noreply.github.com>
2021-10-27 10:18:44 +0200

    Some fixes to ecovelo.py

--- pybikes/ecovelo.py
+++ pybikes/ecovelo.py
@@ -12,9 +12,10 @@ class Ecovelo(BikeShareSystem):
     meta = {
         'system': 'Ecovelo',
         'company': ['Ecovelo'],
+		'ebikes': True #This field is always present in the api, even there are no ebikes in the city, is this a problem?
     }
 
-    BASE_URL = "https://api.cyclist.ecovelo.mobi/v2/stations?&program={dataset}&limit=100"
+    BASE_URL = "https://api.cyclist.ecovelo.mobi/stations?&program={dataset}&limit=100"
 
     def __init__(self, tag, dataset, meta):
         super(Ecovelo, self).__init__(tag, meta)
@@ -22,10 +23,10 @@ class Ecovelo(BikeShareSystem):
 
 
     def update(self, scraper=None):
-        scraper = scraper or utils.PyBikesScraper()
-        data = json.loads(scraper.request(self.feed_url))
-        stations = map(lambda r: r, data['data'])
-        self.stations = list(map(EcoveloStation, stations))
+		scraper = scraper or utils.PyBikesScraper()
+		data = json.loads(scraper.request(self.feed_url))
+		stations = map(lambda r: r, data['data'])
+		self.stations = list(map(EcoveloStation, stations))
 		
 
 class EcoveloStation(BikeShareStation):
@@ -34,10 +35,16 @@ class EcoveloStation(BikeShareStation):
 		latitude = float(fields['position']['latitude'])
 		longitude = float(fields['position']['longitude'])
 		bikes = int(fields['docks']['total_vehicules'])
-		free = int(fields['docks']['total_free'])
+		free = int(fields['statistics']['type']['available']['classic'] + int(fields['statistics']['vehicules']['type']['vae'])
+		#should we add scooter?
+		#free += int(fields['statistics']['vehicules']['type']['scooter'])
 		extra = {
             'status': fields['status'],
             'uid': str(fields['id']),
             'online': fields['status'] == "open",
+			'normal_bikes': int(fields['statistics']['vehicules']['available']['classic']),
+			'ebikes': int(fields['statistics']['vehicules']['available']['vae']),
+			#This field ('has_ebikes') is always present in the api, even there are no ebikes in the city, is this a problem?
+			'has_ebikes': 'vae' in fields['statistics']['vehicules']['available']
         }
 		super(EcoveloStation, self).__init__(name, latitude, longitude, bikes, free, extra)
