98fc2660864361dd316a5923c63064e8c4569676
Altonss <66519591+Altonss@users.noreply.github.com>
2021-12-17 15:26:51 +0100

    Add Ecovelo provider and associated cities (#448)
    * Add ecovelo provider and first data
    
    * Rename ecovelo.py to pybikes/ecovelo.py
    
    * Rename ecovelo.json to pybikes/data/ecovelo.json
    
    * Add Reims(FR)
    
    * Add Niort and Marseillan
    
    * Add Velect'in (Calais (FR))
    
    * Cosmetic changes
    
    * Add Tarbes and Lourdes (FR)
    
    * Add VIA velo (FR)
    
    * Add Vélo'Cité (Laon (FR))
    
    * Add "Le Vélo Baulois" in La baule (FR)
    
    * Add vélYcéo (Saint-Nazaire (FR))
    
    * Add "Le Marcel" in Troyes (FR)
    
    * Add semo in Communauté d'agglomération Seine-Eure(FR)
    
    * Add Cyclovis in Soissons(FR)
    
    * Add Tempo Vélo (Agen (FR))
    
    * Add Saclay and Chantrerie (Captain Bike)
    
    * Fix typo
    
    * Fix typo
    
    * Fix typo
    
    * Some fixes to ecovelo.py

---
+++ pybikes/ecovelo.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2021, Altonss https://github.com/Altonss
+# Distributed under the AGPL license, see LICENSE.txt
+
+import json
+
+from .base import BikeShareSystem, BikeShareStation
+from . import utils
+
+class Ecovelo(BikeShareSystem):
+ 
+    meta = {
+        'system': 'Ecovelo',
+        'company': ['Ecovelo'],
+		'ebikes': True #This field is always present in the api, even there are no ebikes in the city, is this a problem?
+    }
+
+    BASE_URL = "https://api.cyclist.ecovelo.mobi/stations?&program={dataset}&limit=100"
+
+    def __init__(self, tag, dataset, meta):
+        super(Ecovelo, self).__init__(tag, meta)
+        self.feed_url = Ecovelo.BASE_URL.format(dataset=dataset)
+
+
+    def update(self, scraper=None):
+		scraper = scraper or utils.PyBikesScraper()
+		data = json.loads(scraper.request(self.feed_url))
+		stations = map(lambda r: r, data['data'])
+		self.stations = list(map(EcoveloStation, stations))
+		
+
+class EcoveloStation(BikeShareStation):
+    def __init__(self, fields):
+		name = fields['name']
+		latitude = float(fields['position']['latitude'])
+		longitude = float(fields['position']['longitude'])
+		bikes = int(fields['docks']['total_vehicules'])
+		free = int(fields['statistics']['type']['available']['classic'] + int(fields['statistics']['vehicules']['type']['vae'])
+		#should we add scooter?
+		#free += int(fields['statistics']['vehicules']['type']['scooter'])
+		extra = {
+            'status': fields['status'],
+            'uid': str(fields['id']),
+            'online': fields['status'] == "open",
+			'normal_bikes': int(fields['statistics']['vehicules']['available']['classic']),
+			'ebikes': int(fields['statistics']['vehicules']['available']['vae']),
+			#This field ('has_ebikes') is always present in the api, even there are no ebikes in the city, is this a problem?
+			'has_ebikes': 'vae' in fields['statistics']['vehicules']['available']
+        }
+		super(EcoveloStation, self).__init__(name, latitude, longitude, bikes, free, extra)
