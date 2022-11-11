dcb4c75a1ffc6d6b2df0276eb2e80306432b3c6e
eskerda <eskerda@gmail.com>
2022-11-11 11:01:52 +0100

    [gbfs] add a bunch of new fields

--- pybikes/gbfs.py
+++ pybikes/gbfs.py
@@ -153,15 +153,30 @@ class GbfsStation(BikeShareStation):
 
         if 'address' in info:
             self.extra['address'] = info['address']
+        if 'post_code' in info:
+            self.extra['post_code'] = info['post_code']
 
         if 'num_ebikes_available' in info:
             self.extra['has_ebikes'] = True
             self.extra['ebikes'] = int(info['num_ebikes_available'])
 
+        if 'num_bikes_available_types' in info:
+            bike_types = info['num_bikes_available_types']
+            if 'ebike' in bike_types:
+                self.extra['has_ebikes'] = True
+                self.extra['ebikes'] = int(bike_types['ebike'])
+                self.extra['normal_bikes'] = int(bike_types['mechanical'])
+
         if 'rental_methods' in info:
             payment = list(map(unicode.lower, info['rental_methods']))
             self.extra['payment'] = payment
             self.extra['payment-terminal'] = 'creditcard' in payment
+
+        if 'altitude' in info:
+            self.extra['altitude'] = info['altitude']
+
+        if 'capacity' in info:
+            self.extra['slots'] = info['capacity']
 
 
 Gbfs.station_cls = GbfsStation
