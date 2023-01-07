fc488098dd5895649508e39cd742bf42a0fdb17d
eskerda <eskerda@gmail.com>
2023-01-08 00:40:59 +0100

    cleanup fifteen impl

--- pybikes/fifteen.py
+++ pybikes/fifteen.py
@@ -3,23 +3,7 @@
 import re
 import json
 
-from .base import BikeShareSystem, BikeShareStation
-from . import utils
-
-
-class FifteenAPI(BikeShareSystem):
-
-    sync = True
-
-    meta = {
-        'system': 'Fifteen',
-        'company': ['Fifteen SAS']
-    }
-
-    def __init__(self, tag, feed_url, meta):
-        super(FifteenAPI, self).__init__(tag, meta)
-        self.feed_url = feed_url
-
+from pybikes import BikeShareSystem, BikeShareStation, PyBikesScraper
 
 # Each station is formatted as:
 # {
@@ -46,102 +30,53 @@ class FifteenAPI(BikeShareSystem):
 # Extracted from :https://levelo.ampmetropole.fr/api/stations at Marseille, France
 
 
-class LeVelo(FifteenAPI):
+class FifteenAPI(BikeShareSystem):
+
+    sync = True
+
+    meta = {
+        'system': 'Fifteen',
+        'company': ['Fifteen SAS']
+    }
+
+    def __init__(self, tag, feed_url, meta):
+        super(FifteenAPI, self).__init__(tag, meta)
+        self.feed_url = feed_url
+
     def update(self, scraper=None):
-        scraper = scraper or utils.PyBikesScraper()
+        scraper = scraper or PyBikesScraper()
 
         response = json.loads(scraper.request(self.feed_url))
         stations = []
 
-        if response["statusCode"] != 200:
-            return stations
+        if isinstance(response, dict):
+            if response['statusCode'] != 200:
+                raise Exception('response status: %d' % response['statusCode'])
+            data = response['data']['stations']
+        else:
+            data = response
 
-        data = response["data"]["stations"]
+        seen_ids = set()
+
         for s in data:
+            # dedupe by parent_id
+            if s['parent_id'] in seen_ids:
+                continue
+            seen_ids.add(s['parent_id'])
+
             lat = float(s['location']['coordinates'][0])
             lng = float(s['location']['coordinates'][1])
             name = s['label']
             bikes = int(s['info']['number_of_bikes'])
-            free = 10 - bikes
 
-            # Since there is no limit on the number of bikes,
-            # we set it to 0 if the station is full
-            if free < 0:
-                free = 0
-
-            # bike_state_of_charge is not always present
-            if 'bike_state_of_charge' in s['info']:
-                bike_state_of_charge = int(s['info']['bike_state_of_charge'])
-            else:
-                bike_state_of_charge = 0
             extra = {
-                'bike_state_of_charge': bike_state_of_charge,
+                'bike_state_of_charge': int(s['info'].get('bike_state_of_charge', 0)),
                 'bike_autonomy': int(s['info']['bike_autonomy']),
-                'id': s['id'],
+                'uid': s['parent_id'],
                 'distance' : int(s['distance']),
             }
-            station = BikeShareStation(name, lat, lng, bikes, free, extra)
-            stations.append(station)
 
-        self.stations = stations
-
-class Vilvolt(FifteenAPI):
-    def update(self, scraper=None):
-        scraper = scraper or utils.PyBikesScraper()
-
-        data = json.loads(scraper.request(self.feed_url))
-        stations = []
-
-        for s in data:
-            lat = float(s['location']['coordinates'][0])
-            lng = float(s['location']['coordinates'][1])
-            name = s['label']
-            bikes = int(s['info']['number_of_bikes'])
-            free = 10 - bikes
-
-            # Since there is no limit on the number of bikes,
-            # we set it to 0 if the station is full
-            if free < 0:
-                free = 0
-            # bike_state_of_charge is not always present
-            if 'bike_state_of_charge' in s['info']:
-                bike_state_of_charge = int(s['info']['bike_state_of_charge'])
-            else:
-                bike_state_of_charge = 0
-            extra = {
-                'bike_state_of_charge': bike_state_of_charge,
-                'bike_autonomy': int(s['info']['bike_autonomy']),
-                'id': s['id'],
-                'distance' : int(s['distance']),
-            }
-            station = BikeShareStation(name, lat, lng, bikes, free, extra)
-            stations.append(station)
-
-        self.stations = stations
-
-class VeloBaie(FifteenAPI):
-
-    info_url = "https://www.data.gouv.fr/fr/datasets/r/6aba2959-4200-4404-a707-b4954df29fb4"
-
-    def update(self, scraper=None):
-        scraper = scraper or utils.PyBikesScraper()
-
-        info = json.loads(scraper.request(self.info_url))['data']['stations']
-        status = json.loads(scraper.request(self.feed_url))['data']['stations']
-        stations = []
-        nb_stations = len(info)
-        for i in range(nb_stations):
-            if status[i]['is_installed'] == False:
-                continue
-            if info[i]['station_id'] != status[i]['station_id']:
-                continue
-            lat = float(info[i]['lat'])
-            lng = float(info[i]['lon'])
-            name = info[i]['name']
-            bikes = int(status[i]['num_bikes_available'])
-            free = int(status[i]['num_docks_available'])
-            extra = {}
-            station = BikeShareStation(name, lat, lng, bikes, free, extra)
+            station = BikeShareStation(name, lat, lng, bikes, None, extra)
             stations.append(station)
 
         self.stations = stations
