08c5cd5f129617a807a7ce871d2bc4635c3ce73e
eskerda <eskerda@gmail.com>
2023-01-07 23:45:47 +0100

    remove noise copyright headers

--- pybikes/fifteen.py
+++ pybikes/fifteen.py
@@ -1,8 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright (C) 2019, Louis Turpinat <turpinat.louis@gmail.com>
-# Copyright (C) 2016, Lluis Esquerda <eskerda@gmail.com>
-# Copyright (C) 2015, Eduardo Mucelli Rezende Oliveira <edumucelli@gmail.com>
-# Distributed under the AGPL license, see LICENSE.txt
 
 import re
 import json
