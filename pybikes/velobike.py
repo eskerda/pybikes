38e3d6e1db06f797c8051fc8ccbfe80d98a7e445
Martín González Gómez <martin.gonzalez.gomez@nytimes.com>
2023-10-05 21:23:53 -0400

    Add Citybike Lima

--- pybikes/velobike.py
+++ pybikes/velobike.py
@@ -9,12 +9,10 @@ class Velobike(BikeShareSystem):
 
     sync = True
 
-    meta = {
-        'system': 'Velobike',
-        'company': ['Velobike.kz, LLP', 'Smoove']
-    }
+    company = ['Smoove']
 
     def __init__(self, tag, feed_url, meta):
+        meta['company'] += Velobike.company
         super(Velobike, self).__init__(tag, meta)
         self.feed_url = feed_url
 
@@ -25,51 +23,37 @@ class Velobike(BikeShareSystem):
         stations = []
 
         data = json.loads(scraper.request(self.feed_url))
+
         # Some feeds have it inside a list, others within `data`
         if isinstance(data, dict):
-            data = data['data']
-        # Each station is
-        # {
-        #     "id": "41",
-        #     "code": "047",
-        #     "name_ru": "047 Гостиница «Дипломат»",
-        #     "name_en": "047 «Diplomat» hotel",
-        #     "name_kz": "047 «Дипломат» қонақ үйі",
-        #     "lat": "51.130769",
-        #     "lng": "71.429361",
-        #     "desc_ru": "",
-        #     "desc_en": "",
-        #     "desc_kz": "",
-        #     "total_slots": "8",
-        #     "free_slots": "8",
-        #     "avl_bikes": "0",
-        #     "address_ru": "На пересечении ул. Акмешеть, ул.Кунаева.",
-        #     "address_kz": "Ақмешіт пен Қонаев көшелерінің қиылысында",
-        #     "address_en": "At the intersection of Akmeshet str. and Kunaeva street",
-        #     "is_deleted": "0",
-        #     "is_hidden": "0",
-        #     "is_sales": "0",
-        #     "is_not_active": "0"
-        # },
+            data = data.get('data') or data.get('response')
+
         for item in data:
-            if item['is_deleted'] == '1':
+            if 'is_deleted' in item and item['is_deleted'] == '1':
                 continue
-            if item['is_hidden'] == '1':
+            if 'is_hidden' in item and item['is_hidden'] == '1':
                 continue
-            if item['is_sales'] == '1':
+            if 'is_sales' in item and item['is_sales'] == '1':
                 continue
-            if item['is_not_active'] == '1':
+            if 'is_not_active' in item and item['is_not_active'] == '1':
                 continue
-            name = item['name_ru']
-            latitude = float(item['lat'])
-            longitude = float(item['lng'])
+
+            name = item.get('name_ru') or item.get('name')
+            latitude = item.get('lat') or item.get('coordinates')[0]
+            longitude = item.get('lng') or item.get('coordinates')[1]
+            latitude = float(latitude)
+            longitude = float(longitude)
             bikes = int(item['avl_bikes'])
             free = int(item['free_slots'])
             extra = {
-                'uid': int(item['id']),
-                'slots': int(item['total_slots']),
-                'address': item['address_ru'],
+                'uid': item.get('id') or item.get('nid'), 
+                'slots': int(item['total_slots'])
             }
+            extra['uid'] = int(extra['uid'])
+
+            if 'address_ru' in item:
+                extra['address'] = item['address_ru']
+
             station = BikeShareStation(name, latitude, longitude, bikes, free,
                                        extra)
             stations.append(station)
