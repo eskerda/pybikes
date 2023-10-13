803026362eb8768732193fe44beedd5c52108703
eskerda <eskerda@gmail.com>
2023-10-13 14:56:03 +0200

    update callabike
    * use key from argument
    * set feed url from class
    * trust update to reuse headers

--- pybikes/callabike.py
+++ pybikes/callabike.py
@@ -1,60 +1,51 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2023, Martín González Gómez <m@martingonzalez.net>
-# Copyright (C) 2021, Altonss https://github.com/Altonss
 # Distributed under the AGPL license, see LICENSE.txt
 
-from pybikes.utils import keys
 from pybikes.gbfs import Gbfs
+from pybikes.contrib import TSTCache
 
-STATION_INFORMATION = '{feed_url}/station_information'
-STATION_STATUS = '{feed_url}/station_status'
+
+FEED_URL = 'https://apis.deutschebahn.com/db-api-marketplace/apis/shared-mobility-gbfs/2-2/de/CallABike'
+STATION_INFORMATION = FEED_URL + '/station_information'
+STATION_STATUS = FEED_URL + '/station_status'
+
+# caches the feed for 60s
+cache = TSTCache(delta=60)
 
 
 class Callabike(Gbfs):
     authed = True
 
+    # All networks within use the same data feed
+    unifeed = True
+
     meta = {
+        'name': 'Call-A-Bike',
         'system': 'callabike',
-        'company': ['DB'],
+        'company': ['Deutsche Bahn AG'],
     }
 
-    def __init__(self, tag, meta, feed_url, key, bbox):
-        super(Callabike, self).__init__(tag, meta, feed_url)
-        self.bbox = bbox
+    def __init__(self, tag, meta, key, bbox):
+        self.key = key
+        super(Callabike, self).__init__(
+            tag,
+            meta,
+            FEED_URL,
+            bbox=bbox,
+            station_information=STATION_INFORMATION,
+            station_status=STATION_STATUS
+        )
 
     @property
-    def station_information(self):
-        return STATION_INFORMATION.format(feed_url=self.feed_url)
-
-    @property
-    def station_status(self):
-        return STATION_STATUS.format(feed_url=self.feed_url)
-
-    @staticmethod
-    def authorize(scraper):
-        request = scraper.request
-
-        headers = {
-            'DB-Client-Id': keys.client_id,
-            'DB-Api-Key': keys.client_secret,
-            'accept': "application/json"
-        }
-
-        def _request(*args, **kwargs):
-            kwargs['headers'] = headers
-            return request(*args, **kwargs)
-
-        scraper.request = _request
-
-    @property
-    def default_feeds(self):
+    def auth_headers(self):
         return {
-            "station_information": self.station_information,
-            "station_status": self.station_status,
+            'DB-Client-Id': self.key['client_id'],
+            'DB-Api-Key': self.key['client_secret'],
+            'accept': 'application/json',
         }
 
     def update(self, scraper=None):
-        # Patch default scraper request method
-        scraper = scraper or PyBikesScraper()
-        Callabike.authorize(scraper)
+        scraper = scraper or PyBikesScraper(cache)
+        scraper.headers.update(self.auth_headers)
         super(Callabike, self).update(scraper)
