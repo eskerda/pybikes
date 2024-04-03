30cca1c4ffd0127b987bdac49754f04884a7ae06
eskerda <eskerda@gmail.com>
2024-04-03 14:12:25 +0200

    remove cruft in bixi parsers
    Since this parser is only being used by one network and it is expected
    that no other network will use this outdated format we can remove all
    the unused code in there. There was an eval (I myself introduced) in
    there that really serves no purpose

--- pybikes/bixi.py
+++ pybikes/bixi.py
@@ -2,20 +2,9 @@
 # Copyright (C) 2010-2012, eskerda <eskerda@gmail.com>
 # Distributed under the AGPL license, see LICENSE.txt
 
-import json
-import codecs
-
 from lxml import etree
 
 from pybikes import BikeShareSystem, BikeShareStation, PyBikesScraper
-from pybikes.exceptions import StationPlannedException
-
-
-parse_methods = {
-    'xml': 'get_xml_stations',
-    'json': 'get_json_stations',
-    'json_from_xml': 'get_json_xml_stations'
-}
 
 
 def str2bool(v):
@@ -31,40 +20,17 @@ class BixiSystem(BikeShareSystem):
         'company': ['PBSC']
     }
 
-    def __init__(self, tag, feed_url, meta, format):
+    def __init__(self, tag, feed_url, meta):
         super( BixiSystem, self).__init__(tag, meta)
         self.feed_url = feed_url
-        self.method = format
 
     def update(self, scraper = None):
         scraper = scraper or PyBikesScraper()
-        if self.method not in parse_methods:
-            raise Exception(
-                'Extractor for method %s is not implemented' % self.method )
+        xml_data = scraper.request(self.feed_url)
+        dom = etree.fromstring(xml_data.encode('utf-8'))
+        markers = dom.xpath('/stations/station')
+        self.stations = list(map(BixiStation.from_xml, markers))
 
-        self.stations = eval(parse_methods[self.method])(self, scraper)
-
-def get_xml_stations(self, scraper):
-    xml_data = scraper.request(self.feed_url)
-    dom = etree.fromstring(xml_data.encode('utf-8'))
-    markers = dom.xpath('/stations/station')
-    return list(map(BixiStation.from_xml, markers))
-
-def get_json_stations(self, scraper):
-    data = json.loads(scraper.request(self.feed_url))
-    stations = []
-    for marker in data['stationBeanList']:
-        try:
-            station = BixiStation.from_json(marker)
-        except StationPlannedException:
-            continue
-        stations.append(station)
-    return stations
-
-def get_json_xml_stations(self, scraper):
-    raw = scraper.request(self.feed_url).decode('unicode-escape')
-    data = json.loads(raw)
-    return list(map(BixiStation.from_json_xml, data))
 
 class BixiStation(BikeShareStation):
     def __init__(self):
@@ -113,101 +79,4 @@ class BixiStation(BikeShareStation):
         if latestUpdateTime is not None and latestUpdateTime != '0':
             station.extra['latestUpdateTime'] = latestUpdateTime
 
-        return station
-
-    @staticmethod
-    def from_json(data):
-        '''
-          {
-            "id":2026,
-            "stationName":"Broadway & W 60 Street",
-            "availableDocks":0,
-            "totalDocks":0,
-            "latitude":40.76915505,
-            "longitude":-73.98191841,
-            "statusValue":"Planned",
-            "statusKey":2,
-            "availableBikes":0,
-            "stAddress1":"Broadway & W 60 Street",
-            "stAddress2":"",
-            "city":"",
-            "postalCode":"",
-            "location":"",
-            "altitude":"",
-            "testStation":false,
-            "lastCommunicationTime":null,
-            "landMark":""
-          }
-        '''
-        station = BixiStation()
-        if data['statusValue'] == 'Planned' or data['testStation']:
-            raise StationPlannedException()
-
-        station.name      = "%s - %s" % (data['id'], data['stationName'])
-        station.longitude = float(data['longitude'])
-        station.latitude  = float(data['latitude'])
-        station.bikes     = int(data['availableBikes'])
-        station.free      = int(data['availableDocks'])
-
-        station.extra = {
-            'uid': int(data['id']),
-            'statusValue': data['statusValue'],
-            'statusKey': data['statusKey'],
-            'stAddress1': data['stAddress1'],
-            'stAddress2': data['stAddress2'],
-            'city': data['city'],
-            'postalCode': data['postalCode'],
-            'location': data['location'],
-            'altitude': data['altitude'],
-            'testStation': data['testStation'],
-            'lastCommunicationTime': data['lastCommunicationTime'],
-            'landMark': data['landMark'],
-            'totalDocks': data['totalDocks']
-        }
-
-        return station
-
-    @staticmethod
-    def from_json_xml(data):
-        """ json marker object translated from xml
-        {
-            "id": "2",
-            "name": "Docklands Drive - Docklands",
-            "terminalName": "60000",
-            "lastCommWithServer": "1375644471147",
-            "lat": "-37.814022",
-            "long": "144.939521",
-            "installed": "true",
-            "locked": "false",
-            "installDate": "1313724600000",
-            "removalDate": {  },
-            "temporary": "false",
-            "public": "true",
-            "nbBikes": "15",
-            "nbEmptyDocks": "8",
-            "latestUpdateTime": "1375592453128"
-        }
-        """
-
-        station = BixiStation()
-
-        station.name = "%s - %s" % (data['terminalName'], data['name'])
-        station.latitude = float(data['lat'])
-        station.longitude = float(data['long'])
-        station.bikes = int(data['nbBikes'])
-        station.free = int(data['nbEmptyDocks'])
-
-        station.extra = {
-            'uid': int(data['id']),
-            'name': data['name'],
-            'terminalName': data['terminalName'],
-            'lastCommWithServer': data['lastCommWithServer'],
-            'installed': str2bool(data['installed']),
-            'locked': str2bool(data['locked']),
-            'installDate': data['installDate'],
-            'removalDate': data['removalDate'],
-            'temporary': str2bool(data['temporary']),
-            'public': str2bool(data['public']),
-            'latestUpdateTime': data['latestUpdateTime']
-        }
         return station
