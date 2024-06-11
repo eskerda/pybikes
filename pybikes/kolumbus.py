f188ae267107bfc1393ad29ceaa8acdeffc62c65
eskerda <eskerda@gmail.com>
2024-06-11 15:27:14 +0200

    fix Bysykkelen (kolumbus)
    URL feed changed with less data

--- pybikes/kolumbus.py
+++ pybikes/kolumbus.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
+# Copyright (C) 2024, eskerda <eskerda@gmail.com>
 # Copyright (C) 2023, Martín González Gómez <m@martingonzalez.net>
 # Distributed under the LGPL license, see LICENSE.txt
 
+import re
 import json
 
 from pybikes import BikeShareSystem, BikeShareStation, PyBikesScraper
 from pybikes.utils import Bounded
 
 
-BASE_URL = 'https://kolumbus-sanntidsskjerm-backend-prod.azurewebsites.net/sanntidapi/parkings/status'
+BASE_URL = 'https://kolumbus-sanntidsskjerm-backend-prod.azurewebsites.net/entur/city-bikes'
 
 
 class Kolumbus(Bounded, BikeShareSystem):
@@ -49,9 +51,13 @@ class KolumbusStation(BikeShareStation):
         # but people can leave bikes close if there is no room
         # this means we could have more bikes than slots: in that case count it as 0
         self.free = max(slots - bikes, 0)
+
+        # ids are like 'YKO:Station:42'
+        number = next(map(int, (re.findall(r'Station:(\d+)$', data['id']))), None)
+
         self.extra = {
             'has_ebikes': True,
             'slots': slots,
             'uid': data['id'],
-            'number': int(data['external_id']),
+            'number': number,
         }
