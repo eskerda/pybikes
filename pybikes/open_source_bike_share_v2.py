9e20a0859d36e5c7bc0e860ff874ec10543dd687
Sveneld <sveneld300@gmail.com>
2025-08-08 20:44:08 +0200

    Opensource Bikesharing v2

--- pybikes/open_source_bike_share_v2.py
+++ pybikes/open_source_bike_share_v2.py
@@ -4,7 +4,6 @@
 
 import json
 
-from pybikes.base import BikeShareSystem, BikeShareStation
 from pybikes import utils
 from pybikes.open_source_bike_share import OpenSourceBikeShare
 
@@ -18,36 +17,8 @@ class OpenSourceBikeShareV2(OpenSourceBikeShare):
 
     def update(self, scraper=None):
         scraper = scraper or utils.PyBikesScraper()
-
-        stations = []
-
         scraper.headers.update({
             'Authorization': 'Bearer %s' % self.key,
         })
 
-        data = json.loads(scraper.request(self.feed_url))
-
-        print(scraper.last_request)
-
-        for station in data:
-            longitude = float(station.get('lon') or station['longitude'])
-            latitude = float(station.get('lat') or station['latitude'])
-
-            name = station['standName']
-            free = None
-            bikes = int(station.get('bikecount') or station['bikeCount'])
-
-            if 'slotcount' in station:
-                free = int(station['slotcount'])
-
-            extra = {
-                'uid': int(station['standId']),
-                'photo': station.get('standPhoto'),
-                'description': station.get('standDescription'),
-            }
-
-            station = BikeShareStation(name, latitude, longitude, bikes, free,
-                                       extra)
-            stations.append(station)
-
-        self.stations = stations
+        super().update(scraper=scraper)
