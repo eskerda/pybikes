9cfb6b101398e4debf4abdbf44ac930ea1389566
eskerda <eskerda@gmail.com>
2025-09-29 10:37:24 +0200

    accept feed url on digitransit

--- pybikes/digitransit.py
+++ pybikes/digitransit.py
@@ -3,7 +3,7 @@ from pybikes.utils import Bounded
 from pybikes.otp import OTP
 
 
-FEED_URL = "https://api.digitransit.fi/routing/v2/finland/gtfs/v1"
+DEFAULT_FEED_URL = "https://api.digitransit.fi/routing/v2/finland/gtfs/v1"
 
 
 class Digitransit(Bounded, OTP):
@@ -12,8 +12,8 @@ class Digitransit(Bounded, OTP):
 
     meta = {}
 
-    def __init__(self, tag, meta, key, bbox=None):
-        super().__init__(tag, meta, feed_url=FEED_URL, bounds=bbox)
+    def __init__(self, tag, meta, key, feed_url=DEFAULT_FEED_URL, bbox=None):
+        super().__init__(tag, meta, feed_url=feed_url, bounds=bbox)
         self.key = key
 
     def update(self, scraper=None):
